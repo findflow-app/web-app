ec6a07718d0840f6462b6cc10b550ba2b8b64732
TenViki <viktorkomarek28@gmail.com>
2024-11-28 11:58:11 +0100

    :sparkles: Adds prettierrc

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,26 +1,28 @@
-import { Button, Center, Fieldset, Flex, Paper, Stack, TextInput, Title } from '@mantine/core'
-import React from 'react'
+import { Button, Center, Fieldset, Flex, Paper, Stack, TextInput, Title } from "@mantine/core";
+import React from "react";
 
 const LoginPage = () => {
   return (
     <Center mt={32}>
-      <Flex direction={"column"} w={500} >
+      <Flex direction={"column"} w={500}>
         <Paper withBorder p={16}>
-          <Title sx={{
-            color: "blue",
-}}>Login</Title>
+          <Title
+            sx={{
+              color: "blue",
+            }}
+          >
+            Login
+          </Title>
 
           <Stack>
             <TextInput label="Email" placeholder="Your email" />
             <TextInput label="Password" placeholder="Your password" type="password" />
-            <Button>
-              Login
-            </Button>
+            <Button>Login</Button>
           </Stack>
         </Paper>
       </Flex>
     </Center>
-  )
-}
+  );
+};
 
-export default LoginPage
+export default LoginPage;
