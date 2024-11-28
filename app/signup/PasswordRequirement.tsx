ef8fd1ea2d34f3eeb61ddf4d198b1ed5b03355d1
Poznavatelcz <daniel.zivotsky@outlook.cz>
2024-11-28 12:44:14 +0100

    add password requirement component

--- app/signup/PasswordRequirement.tsx
+++ app/signup/PasswordRequirement.tsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
 import { PasswordInput, Progress, Text, Popover, Box, rem } from "@mantine/core";
+import { Check, X } from "lucide-react";
 
 function PasswordRequirement({ meets, label }: { meets: boolean; label: string }) {
   return (
     <Text c={meets ? "teal" : "red"} style={{ display: "flex", alignItems: "center" }} mt={7} size="sm">
-      {meets ? <IconCheck style={{ width: rem(14), height: rem(14) }} /> : <IconX style={{ width: rem(14), height: rem(14) }} />}{" "}
+      {meets ? <Check style={{ width: rem(14), height: rem(14) }} /> : <X style={{ width: rem(14), height: rem(14) }} />}{" "}
       <Box ml={10}>{label}</Box>
     </Text>
   );
